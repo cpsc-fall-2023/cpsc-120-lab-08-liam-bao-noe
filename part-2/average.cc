3e8ba92f8a829a808fc7c7f0aa10103aa2a71088
ldejong20 <142843137+ldejong20@users.noreply.github.com>
2023-11-01 20:27:31 +0000

    initial code written, debugging still needed

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,31 +1,43 @@
-// TODO: Add the required header
+// Bao Nguyen
+// 23bnguyen1@csu.fullerton.edu
+// @23bnguyen1
+// Partners: @ldejong20 @FullNoe
 
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv, argv + argc};
-
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
-
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
-
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
-
-  return 0;
+std::vector<std::string> arguments{argv, argv + argc};
+if (arguments.size() < 2) {
+std::cout << "error: you must supply at least one number\n";
+return 1;
+}
+// TODO: Validate that there is at least one command line argument.
+// If not, print an error message and return a non-zero value.
+// TODO: Write a for-each loop to sum (add up) all of the command line
+// arguments.
+// Use a double or float type so that your program preserves fractional
+// values.
+// The loop needs to skip over the command name, which is the first element
+// of the arguments vector.
+// Each argument is a std::string. You will need to convert each string into
+// a number with the std::stod or std::stof function.
+for (std::string argument : arguments) {
+double num{0.0};
+num = std::string(stoi(arguments));
+double sum_of_total_numbers{0.0};
+sum_of_total_numbers{argument + argument + argument};
+return 0;
+}
+// TODO: After the loop has finished summing the arguments, calculate the
+// average of the values. Recall that the average is the total value divided
+// by the number of values.
+double total_num{0.0};
+double average{sum_of_total_numbers / total_num};
+// TODO: Use cout to print out a message of the form
+// average = *AVERAGE*
+// on its own line.
+std::cout << "Average = " << average << "\n";
+return 0;
 }
